9750c5e686e9a8006622f974e1c93c8024175b45
xpy <pavloschris@gmail.com>
2019-09-08 16:45:46 +0300

    I have no idea what I'm doing!

--- js/actor_view.js
+++ js/actor_view.js
@@ -1,43 +1,85 @@
-
-var actor_view = new Vue({
-    el: '#actor-view',
-    data: {
-        actor_data: {},
-        actor_movies: [],
-        actor_genre_rating: [],
-        current_sorting_key: null,
-        current_sorting_order: null
+Vue.component('actor-view', {
+    data: function () {
+        return {
+            actor_data: {},
+            actor_movies: [],
+            actor_genre_rating: [],
+            current_sorting_key: null,
+            current_sorting_order: null
+        }
+    },
+    mounted: function () {
+        this.$bus.$on('search_form-submit', function (data) {
+            var actor = data.actor;
+            this.load_actor(actor.id);
+        }.bind(this));
     },
     methods: {
         load_actor: function (actor_id) {
-            app.$emit('loading');
-            $.when(
-                callApi(api_get.actor.replace('__id__', actor_id)).done(function (data) {
+            this.$bus.$emit('loading');
+            Promise.all([
+                api.getActor(actor_id).then(data => {
                     this.actor_data = data;
-                }.bind(this)),
-                callApi(api_get.actor_movies.replace('__id__', actor_id)).done(function (data) {
-                    this.actor_movies = data;
-                }.bind(this)),
-                callApi(api_get.actor_genre_rating.replace('__id__', actor_id)).done(function (data) {
-                    this.actor_genre_rating = data;
-                }.bind(this))
-            ).then(
+                }),
+                api.getGenres().then(data => {
+                    this.genres = {};
+                    for (let genre of data.genres) {
+                        this.genres[genre.id] = genre.name;
+                    }
+                }),
+                api.getActorMovies(actor_id).then(data => {
+                    let today = new Date();
+                    this.actor_movies = data.cast.filter(x =>
+                        x.character
+                        && !['himself', 'self'].includes(x.character.toLowerCase())
+                        && new Date(x.release_date) < today);
+                    console.log(this.actor_movies);
+                    for (let movie of this.actor_movies) {
+                        movie['release_year'] = (movie.release_date || '').split('-')[0]
+                    }
+                }),
+                // callApi(api_get.actor_genre_rating.replace('__id__', actor_id)).done(function (data) {
+                //     this.actor_genre_rating = data;
+                // }.bind(this))
+            ]).then(
                 function () {
-                    app.$emit('done-loading');
-                    app.$emit('actor-loaded');
-                }
+                    this.calcGenres();
+                    this.$bus.$emit('done-loading');
+                    this.$bus.$emit('actor-loaded', this.actor_data);
+                }.bind(this)
             )
 
+        },
+        calcGenres: function () {
+            let genres = {};
+            for (let movie of this.actor_movies) {
+                for (let genreId of movie.genre_ids) {
+                    genres[genreId] = genres[genreId] || [];
+                    genres[genreId].push({voteAverage: movie.vote_average, voteCount: movie.vote_count})
+                }
+            }
+            for (let genre in genres) {
+                genres[genre] = {
+                    name: this.genres[genre],
+                    rating: genres[genre].reduce((a, b) => a + b.voteAverage, 0) / genres[genre].length,
+                    filmCount: genres[genre].length
+                }
+            }
+
+            this.actor_genre_rating = genres;
+            console.log(genres)
         },
         // TODO add movie count as well
         average: function () {
             return this.actor_movies.reduce(function (total, movie) {
-                    return total + movie.rating
-                }, 0) / this.actor_movies.length;
+                return total + movie.vote_average
+            }, 0) / this.actor_movies.length;
+        },
+        movie_count_by_bp: function (bp) {
+            return this.actor_movies.filter((x) => Number(x.billing_position) <= bp).length
         },
         sort_movies: function (key) {
-            console.log(key);
-            if (['name', 'votes', 'year', 'role', 'billing_position', 'rating'].indexOf(key) >= 0) {
+            if (['original_title', 'vote_count', 'release_year', 'character', 'billing_position', 'vote_average'].indexOf(key) >= 0) {
                 var self = this;
                 if (key === this.current_sorting_key) {
                     this.current_sorting_order *= -1;
@@ -58,8 +100,8 @@ var actor_view = new Vue({
 
             var sum = 0, amount = 0;
             this.actor_movies.forEach(function (movie) {
-                sum += movie.rating * movie.votes;
-                amount += movie.votes;
+                sum += movie.vote_average * movie.vote_count;
+                amount += movie.vote_count;
             });
             return sum / amount;
         },
@@ -69,15 +111,10 @@ var actor_view = new Vue({
                 return billingPosition > 0 && billingPosition <= position;
             });
             return movies.reduce(function (total, movie) {
-                    return total + movie.rating
-                }, 0) / movies.length;
-
+                return total + movie.rating
+            }, 0) / movies.length;
         }
     }
 
 });
 
-app.$on('search_form-submit', function (data) {
-    var actor = data.actor;
-    actor_view.load_actor(actor.id);
-});
--- js/app.js
+++ js/app.js
@@ -1,58 +1,70 @@
-var api_root = 'https://actorata-xpy.rhcloud.com/api/';
-
-var api_get = {
-    actor_list: 'actor/list',
-    actor: 'actor/__id__',
-    actor_movies: 'actor/__id__/movies',
-    actor_genre_rating: 'actor/__id__/genre_rating'
+let api = {
+    root: 'https://api.themoviedb.org/3/',
+    endPoints: {
+        actor_list: 'person/popular',
+        actor: 'person/__id__',
+        actor_movies: 'person/__id__/movie_credits',
+        movie_genres: 'genre/movie/list',
+        actor_genre_rating: 'actor/__id__/genre_rating',
+        search_person: 'search/person'
+    },
+    callApi: function (url, query_params) {
+        url += '?api_key=b0e628aceae4399d1032199a4bfb5894';
+        query_params = query_params || {};
+        for (let q in query_params) {
+            url += `&${q}=${query_params[q]}`
+        }
+        return fetch(this.root + url, {
+            mode: 'cors',
+            headers: {
+                "Content-Type": "application/json"
+            },
+        }).then(response => response.json())
+    },
+    getActor: function (actorId) {
+        return this.callApi(this.endPoints.actor.replace('__id__', actorId))
+    },
+    getActorMovies: function (actorId) {
+        return this.callApi(this.endPoints.actor_movies.replace('__id__', actorId))
+    },
+    getGenres: function () {
+        return this.callApi(this.endPoints.movie_genres)
+    },
+    searchPeople: function (searchString) {
+        return this.callApi(this.endPoints.search_person, {query: searchString})
+    }
 };
 
-var callApi = function (url) {
-    return $.ajax({
-        crossDomain: true,
-        dataType: 'json',
-        url: api_root + url
-    })
-};
+Vue.prototype.$bus = new Vue({});
 
-var $content = $('.content');
 Vue.filter('round', function (value, decimals) {
     return Number(value).toFixed(decimals);
 });
 
 let app = new Vue({
     el: '#app',
+    data: {
+        contentClasses: new Set(['skata'])
+    },
     created: function () {
-        this.$on('loading', function () {
-            $content.addClass('is-loading');
-        });
-        this.$on('done-loading', function () {
-            $content.removeClass('is-loading');
-        });
-        this.$on('actor-loaded', function () {
-            $content.removeClass('is-not-actor-loaded');
-            $content.addClass('is-actor-loaded');
-            this.changeHash(actor_view.actor_data);
-        });
+        this.$bus.$on('loading', function () {
+            this.contentClasses.add('is-loading');
+        }.bind(this));
+        this.$bus.$on('done-loading', function () {
+            this.contentClasses.delete('is-loading');
+        }.bind(this));
+        this.$bus.$on('actor-loaded', function (actor_data) {
+            this.contentClasses.add('is-not-actor-loaded');
+            this.contentClasses.add('is-actor-loaded');
+            this.changeHash(actor_data);
+        }.bind(this));
 
     }, methods: {
         getActorHashName: function (actor) {
-            return actor.fname + '-' + actor.lname;
+            return actor.name.replace(' ', '-');
         },
         changeHash: function (actor) {
             window.location.hash = this.getActorHashName(actor);
         },
-        getFromHash: function () {
-            let hash = window.location.hash.replace('#', '');
-            for (let actor of search_field.actor_list) {
-                if (hash === this.getActorHashName(actor)) {
-                    return actor;
-                } else if (hash === String(actor.id)) {
-                    this.changeHash(actor);
-                    return actor;
-                }
-            }
-            return null;
-        }
     }
 });
