32de119fe12f48c63f708a611c29677ddc190da8
xpy <pavloschris@gmail.com>
2022-09-24 20:49:46 +0300

    add images, pages in search

--- js/main.js
+++ js/main.js
@@ -3,7 +3,8 @@ require.config(
         paths: {
             Vue: 'vendor/vue',
             Chart: '//cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.bundle.min',
-            Api: 'base-api'
+            Api: 'api',
+            Fuse: '//cdn.jsdelivr.net/npm/fuse'
         }
     },
 );
